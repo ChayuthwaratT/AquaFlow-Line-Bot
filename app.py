8d34cc810ca57738439fa18f20482b7f75e86904
ChayuthwaratT <chayuthwarat@gmail.com>
2026-09-05 09:46:44 +0700

    move url to env

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-﻿from flask import Flask, request, abort, render_template
+from flask import Flask, request, abort, render_template
 from dotenv import load_dotenv  # type: ignore
 
 from db import (
